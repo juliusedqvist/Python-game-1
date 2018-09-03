358fa08898214fcb90d2b0c16c2f20a19b3d14c6
juliusedqvist <36112602+juliusedqvist@users.noreply.github.com>
2018-09-03 16:33:01 +0200

    Update intro.py

--- intro.py
+++ intro.py
@@ -9,27 +9,27 @@ def intro():
 
 def introText():
     # Suspect.set_difficulty()
-    skipIntro = input("Want to skip intro?")
-    if skipIntro == "yes":
-        gameplayStart()
-    else:
-        time.sleep(3)
-        print("You are a detective in the 10th century.\n")
-        time.sleep(5)
-        print("In the middle of the dinner a page stumbles through your door.\n")
-        time.sleep(5)
-        print("""There's been a murder! The king is dead!
+    #skipIntro = input("Want to skip intro?")
+    #if skipIntro == "yes":
+    #    gameplayStart()
+    #else:
+    time.sleep(3)
+    print("You are a detective in the 10th century.\n")
+    time.sleep(5)
+    print("In the middle of the dinner a page stumbles through your door.\n")
+    time.sleep(5)
+    print("""There's been a murder! The king is dead!
 Can you help us find the one guilty of this horrible deed?\n""")
-        time.sleep(5)
-        print("""You follow the page through the city and up to the castle and into a secluded room. In the room there
+    time.sleep(5)
+    print("""You follow the page through the city and up to the castle and into a secluded room. In the room there
 are five others. The Queen, A noble, the local priest, a chef and a blacksmith.
 The page says that these are the suspects and only one of them is guilty but the others could be involved in several
 ways and you have to find out what happened and who's guilty of the deed by questioning them.\n""")
-        time.sleep(10)
-        print("""First you pick who to question, then you can ask the suspect about other suspects including the one
+    time.sleep(10)
+    print("""First you pick who to question, then you can ask the suspect about other suspects including the one
 you're talking to. You can also ask them about their relationship with the king by simply typing king. \n""")
-        time.sleep(10)
-        gameplayStart()
+    time.sleep(10)
+    gameplayStart()
 
 
 def gameplayStart():
